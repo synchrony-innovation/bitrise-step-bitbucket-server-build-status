510b7767d846b53716bde78cd262738f36703314
Stu Levine <stulevine@users.noreply.github.com>
2019-12-09 15:54:28 -0500

    Update step.sh
    made STATE dynamic based on BITRISE_BUILD_STATUS environment variable

--- step.sh
+++ step.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 
-## assumes that the input 'state' variable was set by the user prior to using this Step
-##
-## 'state' can be "FAILED", "INPROGRESS" or "SUCCESSFUL"
+## STATE can be "FAILED", "INPROGRESS" or "SUCCESSFUL"
+
+## BITRISE_BUILD_STATUS
+##     The current status of the build. The available options are:
+##        Not finished (0)
+##        Successful (1)
+##        Failed (2)
+##        Aborted with failure (3)
+##        Aborted with success (4)
+
+case $BITRISE_BUILD_STATUS in
+
+  0)
+    STATE="INPROGRESS"
+    ;;
+
+  1)
+    STATE="SUCCESSFUL"
+    ;;
+
+  *)
+    STATE="FAILED"
+    ;;
+esac
 
 ## Request
 curl -X "POST" "https://bitbucket-dev.glb.syfbank.com/rest/build-status/1.0/commits/${BITRISE_GIT_COMMIT}" \
 -H 'Content-Type: application/json' \
 -d $'{
-"state": "${state}",
+"state": "${STATE}",
 "key": "syb-ios-ignite",
 "name": "$BITRISE_GIT_BRANCH",
 "url": "${BITRISE_BUILD_URL}",
