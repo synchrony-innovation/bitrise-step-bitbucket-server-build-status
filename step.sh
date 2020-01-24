6adf54f3db74aeabb2055f72615710409bc59ca6
Stu Levine <stulevine@users.noreply.github.com>
2020-01-24 14:17:36 -0500

    Update step.sh
    - updated script to fix environment variable not evaluating in single suites.
    - updated BITRISE_BUILD_STATUS possible values from docs here https://devcenter.bitrise.io/builds/available-environment-variables/

--- step.sh
+++ step.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# fail if any commands fails
+set -e
+# debug log
+set -x
 
 ## input: bitbucket_base_url - is the base url for the bitbucket build-status update post.
 
@@ -6,20 +10,16 @@
 
 ## BITRISE_BUILD_STATUS
 ##     The current status of the build. The available options are:
-##        Not finished (0)
-##        Successful (1)
-##        Failed (2)
-##        Aborted with failure (3)
-##        Aborted with success (4)
+##        Successful (0)
+##        Failed (1)
 
-case $BITRISE_BUILD_STATUS in
-
+case ${BITRISE_BUILD_STATUS} in
   0)
-    STATE="INPROGRESS"
+    STATE="SUCCESSFUL"
     ;;
 
   1)
-    STATE="SUCCESSFUL"
+    STATE="FAILED"
     ;;
 
   *)
@@ -27,13 +27,15 @@ case $BITRISE_BUILD_STATUS in
     ;;
 esac
 
+echo "Current build status: (${BITRISE_BUILD_STATUS}) $STATE"
+
 ## Request
-curl -X "POST" "${bitbucket_base_url}/rest/build-status/1.0/commits/${BITRISE_GIT_COMMIT}" \
+curl -u "${svc_username}:${svc_password}" -X "POST" "https://api.syf.com/v1/bitrise/bitbucket1/rest/build-status/1.0/commits/${BITRISE_GIT_COMMIT}" \
 -H 'Content-Type: application/json' \
--d $'{
-"state": "${STATE}",
-"key": "syb-ios-ignite",
-"name": "$BITRISE_GIT_BRANCH",
-"url": "${BITRISE_BUILD_URL}",
-"description": "$BITRISE_GIT_MESSAGE"
-}'
+-d "{
+\"state\": \"${STATE}\",
+\"key\": \"syb-ios-ignite\",
+\"name\": \"$BITRISE_GIT_BRANCH\",
+\"url\": \"${BITRISE_BUILD_URL}\",
+\"description\": \"$BITRISE_GIT_MESSAGE\"
+}"
